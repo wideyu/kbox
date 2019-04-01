a6925943a41b710d2830a17d7e0b75b7e1b4aa01
wideyu <wideyu@gmail.com>
2019-04-01 18:48:29 +0800

    Create fetchPromise.js

---
+++ fetchPromise.js
@@ -0,0 +1,6 @@
+<script src="https://github.com/github/fetch/releases/download/v3.0.0/fetch.umd.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.min.js"></script>
+
+if (!windows.fetch) {
+  console.log('noFetch');
+}
